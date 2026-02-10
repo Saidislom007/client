e41325f17d5deedc3ed4eaf5c4d2e77fc50b8167
Saidislom <saidislomabdugafforov7@gmail.com>
2026-02-10 09:49:10 +0500

    Fix formatting in vite.config.js
    Removed unnecessary comma in plugins array and formatted code.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  
+  server: {
+    allowedHosts: ['client-production-1ce9.up.railway.app']
+  },
+
+  preview: {
+    allowedHosts: ['client-production-1ce9.up.railway.app']
+  }
 })
